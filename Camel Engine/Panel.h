e2aef67f3adc18afe23886836cb96731af97fffb
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2020-10-28 17:53:16 +0100

    Added panel header

---
+++ Camel Engine/Panel.h
@@ -0,0 +1,20 @@
+#ifndef __PANEL_H__
+#define __PANEL_H__
+
+
+#include "Globals.h"
+class Panel
+{
+public:
+	Panel() {};
+	Panel(const char* n) {};
+	~Panel() {};
+	virtual update_status Draw() { return UPDATE_CONTINUE; };
+	virtual bool IsEnabled() { return true; };
+
+
+
+protected:
+	bool enabled;
+};
+#endif
